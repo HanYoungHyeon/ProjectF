ce49df013bd5e14e01f7b8b15c793c620f60fde4
HanYoungHyeon <112858988+HanYoungHyeon@users.noreply.github.com>
2022-11-09 19:34:28 +0900

    YH Player이동
    Feat : 플레이어 이동 임시 구현

--- Assets/YH/Scripts/Player.cs
+++ Assets/YH/Scripts/Player.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+    private CharacterController character;
+    private float axisX;
+    private float axisZ;
+    private float moveSpeed;
+    private void Awake()
+    {
+        moveSpeed = 5f;
+        character = GetComponent<CharacterController>();
+    }
 
+    private void Update()
+    {
+        Move();
+        
+    }
+    private void Move()
+    {
+        axisX = Input.GetAxis("Horizontal");
+        axisZ = Input.GetAxis("Vertical");
+        Vector3 moveInput = new Vector3(axisX, 0, axisZ);
+        character.Move(moveInput * moveSpeed * Time.deltaTime);
+    }
 }
