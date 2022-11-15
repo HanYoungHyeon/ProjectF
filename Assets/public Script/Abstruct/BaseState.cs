54152d8389d31798c61b836f679bb4c465eab344
Choi <63259644+bananasplif777@users.noreply.github.com>
2022-11-15 18:01:28 +0900

    fix : 충돌 해결

--- Assets/public Script/Abstruct/BaseState.cs
+++ Assets/public Script/Abstruct/BaseState.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 
 public abstract class BaseState : IStater
 {
-<<<<<<< HEAD
-    public GameObject gameObject;
-    public BaseState(GameObject gameObject)
-    {
-        this.gameObject = gameObject;
-=======
     public GameObject gameObj;
     public BaseState(GameObject game)
     {
         this.gameObj = game;
->>>>>>> YH
     }
     public abstract void Enter();
     public abstract void Exit();
-<<<<<<< HEAD
 
-=======
->>>>>>> YH
     public abstract void Update();
 }
